274654bd52c60ccbaec428ef154495117508cb26
aralmheiri <aralmheiri@mail.csuchico.edu>
2018-03-01 19:47:38 -0800

    secretroom

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: aralmheiri
 #include "SecretDoor/SecretDoor.h"
 #include <iostream>
 
@@ -6,6 +7,21 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+ int whole_number;
+ int winner;
+      cout<<"how many times do you want to play?\n";
+      cin>>whole_number;
+       for( int index=0; index<whole_number; index++)
+       {
+        game.newGame();
+        game.guessDoorC();
+        game.guessDoorC();
+       if(game.isWinner() == true)
+       {
+        winner++;
+       }
+       }
+       cout<<"how many times you won?\n"<<winner<<endl;
+
   return 0;
 }
